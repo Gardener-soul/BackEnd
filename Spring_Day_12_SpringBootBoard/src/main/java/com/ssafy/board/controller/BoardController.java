e637fe23b8301a31d828b58b5a349840668fbeba
Gardener <81845773+Gardener-soul@users.noreply.github.com>
2023-10-24 12:41:44 +0900

    Add files via upload

---
+++ Spring_Day_12_SpringBootBoard/src/main/java/com/ssafy/board/controller/BoardController.java
@@ -0,0 +1,116 @@
+package com.ssafy.board.controller;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.ssafy.board.model.dto.Board;
+import com.ssafy.board.model.dto.SearchCondition;
+import com.ssafy.board.model.service.BoardService;
+
+@Controller
+public class BoardController {
+	
+	// 의존성 주입
+	@Autowired
+	private BoardService boardService;
+	
+//	@RequestMapping(value = "/", method = RequestMethod.GET)
+	@GetMapping("/")
+	public String index() {
+		// 서비스 -> Dao -> DB -> 쭉 뿌려준다.
+		
+		// 현재로서는 index.jsp 가 없어서 (대문 페이지) 바로 리스트로 요청을 다시 하게끔 돌려보냄
+		return "redirect:list";
+	}
+	
+	@GetMapping("list")
+	public String list(Model isfp) {
+		
+		List<Board> list = boardService.getList();
+		isfp.addAttribute("list",list);
+		
+		return "/board/list";
+	}
+	
+	@GetMapping("detail")
+	public String detail(Model model, int id) throws SQLException {
+		
+		Board board = boardService.getBoard(id);
+		model.addAttribute("board", board);
+		
+		return "/board/detail";
+	}
+	
+	@GetMapping("delete")
+	public String delete(int id) {
+		boardService.removeBoard(id);
+		return "redirect:list";
+	}
+	
+	@GetMapping("writeform")
+	public String writeform() {
+		
+		return"/board/writeform";
+	}
+	
+	@PostMapping("write")
+	public String write(Board board) {
+//		System.out.println("DB 다녀오기 전" + board);
+		boardService.writeBoard(board);
+//		System.out.println("DB 다녀온 후" + board);
+		return "redirect:detail?id="+board.getId();
+	}
+	
+	@GetMapping("updateform")
+	public String updateform(Model model, int id) throws SQLException {
+		// id 게시글 하나를 가져와서 모델에 실어놓고 폼으로 보내겠다.
+		// 현재로써는 게시글을 하나 얻어오면 조회수가 증가해버리지만 이건 잘못된 것.
+		model.addAttribute("board", boardService.getBoard(id));
+		return "/board/updateform";
+	}
+	
+	@PostMapping("update")
+	public String update(Board board) {
+		boardService.modifyBoard(board);
+		return "redirect:detail?id="+board.getId();
+	}
+	
+	@GetMapping("search")
+//	public String search(Model model, String key, String word, String orderBy, String orderByDir) {
+	public String search(Model model, SearchCondition condition) {
+ 		model.addAttribute("list", boardService.search(condition));
+		return "/board/list";
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ Spring_Day_12_SpringBootBoard/src/main/webapp/WEB-INF/views/board/detail.jsp
@@ -0,0 +1,31 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>게시글 상세보기</title>
+<%@ include file="../common/bootstrap.jsp" %>
+</head>
+<body>
+	<div class="container">
+		<h2>글상세보기</h2>
+		<hr>
+		<div class = "card">
+			 <h2 class="card-title">${board.title} <span class="badge text-bg-danger">${board.viewCnt}</span></h2>
+    		 	<div class="d-flex justify-content-between">
+    		 		<div class = "card-subtitle">${board.writer}</div>
+    		 		<div class = "card-subtitle">${board.regDate}</div>
+    		 	</div>
+    		 <p class="card-text">${board.content}</p>
+    		 
+    		 <div>
+    		 	<a href="delete?id=${board.id}" class="btn btn-outline-danger">삭제</a>
+    		 	<a href="updateform?id=${board.id}" class="btn btn-outline-success">수정</a>
+    		 	<a href= "list" class="btn btn-outline-primary">목록</a>
+    		 </div>
+    		 		
+		</div>
+	</div>
+</body>
+</html>
